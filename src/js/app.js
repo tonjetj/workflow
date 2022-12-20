f33efd9866e473860d084463cd5cbeb61083f5fe
tonjetj <tontotjen@gmail.com>
2022-12-20 19:40:47 +0100

    function

---
+++ src/js/app.js
@@ -0,0 +1,9 @@
+const apiURL = "http://nf-api.onrender.com/api/v1/";
+const catFacts = "cat-facts/";
+const url = apiURL + catFacts;
+
+export async function app() {
+  //request
+  //render
+  return "App Ready!";
+}
